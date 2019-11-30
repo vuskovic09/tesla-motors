f5795a86c3f2b56113420dc23457188b27304520
Filip Vušković <filip.vuskovic09@gmail.com>
2019-11-30 20:00:25 +0100

    MOD: css + js - nav and social nav

--- js/script.js
+++ js/script.js
@@ -1,36 +1,45 @@
 $(document).ready(function(){
-  $('.dropdownLink').hover(function(e) {
-    e.preventDefault();
-    $('#dropdown').slideDown(500); 
-  }, function() {
-    $('#dropdown').mouseleave(function(){
-      $('#dropdown').slideUp(500);
-    });
-  });
-
-  $("#socialButton").click(function(e){
-    e.preventDefault();
-    let menu = $("#socialMenu");
-    if(menu.hasClass("opened")){
-      menu.removeClass("opened");
-      $("#socialButton").css({
-        "border-bottom-right-radius": "5px"
-      });
-      menu.slideUp(500);
-    } 
-    else {
-      menu.addClass("opened");
-      $("#socialButton").css({
-        "border-bottom-right-radius": "0px"
-      });
-      $("#socialMenu").css({
-        "border-bottom-right-radius": "5px"
-      });
-      menu.slideDown(500);
-    } 
-  });
+  navHover();
+  socialMenu();
 });
 
 function socialMenu(){
-  
+    let menu = $("#socialMenu");
+    let button = $("#socialButton")
+
+    button.click(function(){
+      if(menu.hasClass("opened")){
+        menu.removeClass("opened");
+        setTimeout(function(){
+          button.css({
+          "border-bottom-right-radius": "5px"
+          });
+        }, 500);
+        menu.slideUp(500);
+      } 
+      else {
+        menu.addClass("opened");
+        button.css({
+          "border-bottom-right-radius": "0px"
+        });
+        menu.css({
+          "border-bottom-right-radius": "5px"
+        });
+        menu.slideDown(500);
+      }
+    });
+}
+
+function navHover(){
+  let ddLink = $(".dropdownLink");
+  let ddList = $("#dropdown");
+  ddLink.hover(function(e) {
+    e.preventDefault();
+    ddList.slideDown(500); 
+  }, function(e) {
+    event.preventDefault();
+    ddList.mouseleave(function(){
+      ddList.slideUp(500);
+    });
+  });
 }
