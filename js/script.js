c941c3fc8a6e5a95de42135737e4f0ee2543b149
Filip Vušković <filip.vuskovic09@gmail.com>
2019-12-03 15:42:59 +0100

    MOD: js + css optimization

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   navHover();
+  heroImages();
   paragraphShow();
   paragraphHide();
-  heroImages();
 
   //SLICK PLUGIN START
   $('.single-item').slick({
@@ -16,32 +16,6 @@ $(document).ready(function(){
   //SLICK PLUGIN END
 });
 
-function paragraphShow(){
-    let para = $("#hiddenPara");
-    let button = $("#showMore");
-    let buttonHide = $('#hide')
-
-    button.click(function(){
-      para.addClass("opened");
-      para.slideDown(500);
-      button.slideUp(600);
-      buttonHide.slideDown(600);
-      buttonHide.css('display', 'inline-block');
-    });
-}
-
-function paragraphHide(){
-  let para = $('#hiddenPara');
-  let button = $('#showMore');
-  let buttonHide = $('#hide');
-  
-  buttonHide.click(function(){
-    para.removeClass("opened");
-    para.slideUp(500);
-    button.slideDown(600);
-    buttonHide.slideUp(600);
-  });
-}
 
 function navHover(){
   let ddLink = $(".dropdownLink");
@@ -62,7 +36,54 @@ function heroImages(){
   let hero = $('#hero');
   let ispis = '';
   for(i = 0; i < images.length; i++) {
-    ispis += '<img class="heroIMG" src="images/' + images[i] + '.jpg" alt="Hero image"' + i + '/>'
+    ispis += '<img class="heroIMG" src="images/' + images[i] + '.jpg" alt="Hero image' + i + '"/>';
   };
   hero.html(ispis);
 }
+
+function aboutParagraph(){
+  let para = $('#hiddenPara');
+  let showButton = $('#showMore');
+  let hideButton = $('#hide');
+
+  showButton.click(function(){
+    para.slideDown(500);
+    showButton.slideUp(600);
+    hideButton.slideDown(600);
+    hideButton.css('display', 'inline-block'); //So it centers
+  });
+
+  hideButton.click(function(){
+    para.slideUp(500);
+    showButton.slideDown(600);
+    hideButton.slideUp(600);
+  });
+
+}
+
+// function paragraphShow(){
+//     let para = $("#hiddenPara");
+//     let button = $("#showMore");
+//     let buttonHide = $('#hide')
+
+//     button.click(function(){
+//       para.addClass("opened");
+//       para.slideDown(500);
+//       button.slideUp(600);
+//       buttonHide.slideDown(600);
+//       buttonHide.css('display', 'inline-block');
+//     });
+// }
+
+// function paragraphHide(){
+//   let para = $('#hiddenPara');
+//   let button = $('#showMore');
+//   let buttonHide = $('#hide');
+  
+//   buttonHide.click(function(){
+//     para.removeClass("opened");
+//     para.slideUp(500);
+//     button.slideDown(600);
+//     buttonHide.slideUp(600);
+//   });
+// }
