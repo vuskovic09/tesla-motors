60a157891fdc13aa9ac904ae8a461322d0e7b772
Filip Vušković <filip.vuskovic09@gmail.com>
2019-12-04 15:38:38 +0100

    MOD: array elements

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ function modelInfo(){
   let div = document.getElementById('models');
   let img = ['modelX.jpg', 'modelY.jpg', 'modelS.jpg', 'model3.jpg', 'roadster.jpg', 'cybertruck.jpg']
   let models = ['Model X', 'Model Y', 'Model S', 'Model 3', 'Roadster',  'Cybertruck'];
-  let year = ['2015', '2019', '2012', '2017', '2020', '2020'];
+  let date = ['September 2015', 'November 2019', 'June 2012', 'July 2017', '2020', '2020'];
   let range = ['523km', '483km', '600km', '402km', '997km', '804km'];
   let price = ['$84,990', '$39,000', '$79,990', '$36,000', '$200,000', '$39,900'];
 
@@ -83,7 +83,7 @@ function modelInfo(){
         </tr>
         <tr>
           <td>Date:</td>
-          <td>${year[i]}</td>
+          <td>${date[i]}</td>
         </tr>
         <tr>
           <td>Range:</td>
